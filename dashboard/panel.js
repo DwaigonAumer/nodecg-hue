d88c7fa32c7f7af2aff7f3abbc7e13d8d93cc9c2
Ben Fairclough <LyphoTV@gmail.com>
2016-02-21 14:54:24 -0500

    feat(panel): add preview for selected color

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -8,18 +8,38 @@ else {
    nodecg.sendMessageToBundle('searchForBridge', 'nodecg-hue');
 }
 
+// global elements
+var colorPreview, rInput, gInput, bInput, rSlider, gSlider, bSlider;
+
 document.addEventListener('WebComponentsReady', function() {
-   document.querySelector('.red input#input').addEventListener('change', function() {sendRgbValueChanged();})
-   document.querySelector('.green input#input').addEventListener('change', function() {sendRgbValueChanged();})
-   document.querySelector('.blue input#input').addEventListener('change', function() {sendRgbValueChanged();})
-   document.querySelector('.red #sliderContainer').addEventListener('click', function() {sendRgbValueChanged();})
-   document.querySelector('.green #sliderContainer').addEventListener('click', function() {sendRgbValueChanged();})
-   document.querySelector('.blue #sliderContainer').addEventListener('click', function() {sendRgbValueChanged();})
+   // global elements
+   rInput = document.querySelector('.red input#input');
+   gInput = document.querySelector('.green input#input');
+   bInput = document.querySelector('.blue input#input');
+   rSlider = document.querySelector('.red #sliderContainer');
+   gSlider = document.querySelector('.green #sliderContainer');
+   bSlider = document.querySelector('.blue #sliderContainer');
+   colorPreview = document.querySelector('#color-preview');
+
+   // listeners that will notify server of final color change
+   rInput.addEventListener('change', function() {sendRgbValueChanged();})
+   gInput.addEventListener('change', function() {sendRgbValueChanged();})
+   bInput.addEventListener('change', function() {sendRgbValueChanged();})
+   rSlider.addEventListener('click', function() {sendRgbValueChanged();})
+   gSlider.addEventListener('click', function() {sendRgbValueChanged();})
+   bSlider.addEventListener('click', function() {sendRgbValueChanged();})
+
+   // listeners that will notify color-preview of immediate color changes
+   rInput.addEventListener('bind-value-changed', function() {updatePreviewColor();})
+   gInput.addEventListener('bind-value-changed', function() {updatePreviewColor();})
+   bInput.addEventListener('bind-value-changed', function() {updatePreviewColor();})
 });
 
 function sendRgbValueChanged() {
-   var r = document.querySelector('.red input#input').value;
-   var g = document.querySelector('.green input#input').value;
-   var b = document.querySelector('.blue input#input').value;
-   nodecg.sendMessageToBundle('rgbValueChanged', 'nodecg-hue', {'r': r, 'g': g, 'b': b});
+   nodecg.sendMessageToBundle('rgbValueChanged', 'nodecg-hue', {'r': rInput.value, 'g': gInput.value, 'b': bInput.value});
+}
+
+function updatePreviewColor() {
+   var color = "rgb(" + rSlider.parentElement.immediateValue + ", " + gSlider.parentElement.immediateValue + ", " + bSlider.parentElement.immediateValue + ")";
+   colorPreview.style.backgroundColor = color;
 }
