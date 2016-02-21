6ecaea0174b6cf72315a88946775513f5a9dd25d
Ben Fairclough <LyphoTV@gmail.com>
2016-02-21 12:35:35 -0500

    fix(panel): prevent duplicate messages to server
    grab value from iron-input instead of slider's immediate value, since we're not live updating the lights

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -12,14 +12,14 @@ document.addEventListener('WebComponentsReady', function() {
    document.querySelector('.red input#input').addEventListener('change', function() {sendRgbValueChanged();})
    document.querySelector('.green input#input').addEventListener('change', function() {sendRgbValueChanged();})
    document.querySelector('.blue input#input').addEventListener('change', function() {sendRgbValueChanged();})
-   document.querySelector('.red').addEventListener('click', function() {sendRgbValueChanged();})
-   document.querySelector('.green').addEventListener('click', function() {sendRgbValueChanged();})
-   document.querySelector('.blue').addEventListener('click', function() {sendRgbValueChanged();})
+   document.querySelector('.red #sliderContainer').addEventListener('click', function() {sendRgbValueChanged();})
+   document.querySelector('.green #sliderContainer').addEventListener('click', function() {sendRgbValueChanged();})
+   document.querySelector('.blue #sliderContainer').addEventListener('click', function() {sendRgbValueChanged();})
 });
 
 function sendRgbValueChanged() {
-   var r = document.querySelector('.red').immediateValue;
-   var g = document.querySelector('.green').immediateValue;
-   var b = document.querySelector('.blue').immediateValue;
+   var r = document.querySelector('.red input#input').value;
+   var g = document.querySelector('.green input#input').value;
+   var b = document.querySelector('.blue input#input').value;
    nodecg.sendMessageToBundle('rgbValueChanged', 'nodecg-hue', {'r': r, 'g': g, 'b': b});
 }
